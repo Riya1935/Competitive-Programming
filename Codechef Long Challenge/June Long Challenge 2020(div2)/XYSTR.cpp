64b8a9ad3f499004f08cedbb4797738e06da0000
Riya1935 <riyathakur1935@gmail.com>
2020-06-29 01:37:50 +0530

    Create XYSTR.cpp

---
+++ Codechef Long Challenge/June Long Challenge 2020(div2)/XYSTR.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define ld long double
+#define pb push_back
+#define fi0(i,n) for(int i=0; i<n; i++)
+#define fi1(i,n) for(int i=1; i<n; i++)
+#define fll0(i,n) for(long long i=0; i<n; i++)
+#define fll1(i,n) for(long long i=1; i<n; i++)
+#define vll vector<long long>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+int main()
+  {
+      ll t;
+      cin>>t;
+      while(t--)  {
+      string s;
+      cin>>s;
+      ll i=0;
+      ll count=0;
+      while(i<s.length()-1)
+        {
+            if((s.at(i)=='x' && s.at(i+1)=='y')|| (s.at(i)=='y' && s.at(i+1)=='x'))
+              {
+                  count=count+1;
+                  i=i+2;
+              }
+            else  i=i+1;
+             
+        }
+    cout<<count<<endl;}
+  }
+
