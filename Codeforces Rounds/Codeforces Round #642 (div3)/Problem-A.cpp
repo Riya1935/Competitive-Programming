a0206dcba09335075e48696afdc130621df73ba7
Riya1935 <riyathakur1935@gmail.com>
2020-06-30 21:09:39 +0530

    Create Problem-A.cpp

---
+++ Codeforces Rounds/Codeforces Round #642 (div3)/Problem-A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define l long int
+#define ld  long double
+#define pb push_back
+#define fi0(i,n) for(int i=0; i<n; i++)
+#define fi1(i,n) for(int i=1; i<n; i++)
+#define fll0(i,n) for(long long i=0; i<n; i++)
+#define fll1(i,n) for(long long i=1; i<n; i++)
+#define vll vector<long long>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+#define pll pair<ll,ll>
+#define ull unsigned ll
+int main()
+  {
+     ll t;
+     cin>>t;
+     while(t--)
+       {
+          ll n,m;
+          cin>>n>>m;
+          if(n==1)  cout<<"0"<<endl;
+          else if(n==2)  cout<<m<<endl;
+          else cout<<2*m<<endl;
+          
+       }
+  }
