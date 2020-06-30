4ecd604444ce146fcd7d85252c5756c4816315aa
Riya1935 <riyathakur1935@gmail.com>
2020-06-30 21:15:24 +0530

    Create Problem-C1.cpp

---
+++ Codeforces Educational Rounds/Educational Codeforces Round 87/Problem-C1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define ld long double
+#define pb push_back
+#define fi0(i,n) for(int i=0; i<n; i++)
+#define fi1(i,n) for(int i=1; i<n; i++)
+#define fll0(i,n) for(long long i=0; i<n; i++)
+#define fll1(i,n) for(long long i=1; i<n; i++)
+#define vll vector<long long>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+int main()
+  {
+            ll t;
+            cin>>t;
+            while(t--)
+              {
+                        ll n;
+                        cin>>n;
+                        ld pi=2*acos(0.0);
+                        ld x=pi/(2*n);
+                        cout<<fixed<<setprecision(7)<<1/(tan(x))<<endl;
+              }
+  }
