c654267ee82a039a5c4887c705acdf7fbc60606a
Riya1935 <riyathakur1935@gmail.com>
2020-06-30 21:11:59 +0530

    Create Problem-A.cpp

---
+++ Codeforces Rounds/Codeforces Round #643/Problem-A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define ld long double
+#define pb push_back
+#define fi0(i,n) for(int i=0; i<n; i++)
+#define fi1(i,n) for(int i=1; i<n; i++)
+#define fll0(i,n) for(long long i=0; i<n; i++)
+#define fll1(i,n) for(long long i=1; i<n; i++)
+#define vll vector<long long>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+int main()
+  {
+            ll t;
+            cin>>t;
+            while(t--)
+              {
+                   ll n;
+                   cin>>n;
+                   vll v(n);
+                   fll0(i,n)   cin>>v[i];
+                   sort(v.begin(), v.end());
+                   ll a=*max_element(v.begin(), v.end());
+                   ll arr[a+1]={0};
+                   fll0(i,n)  arr[v[i]]++;
+                   ll count=0;
+                   fll1(i,a+1)
+                     {
+                         if(i!=a) {
+                         count=count+(arr[i]/i);
+                         ll rem=arr[i]%i;
+                         arr[i+1]=arr[i+1]+rem;}
+                         else
+                           {
+                                count=count+(arr[i]/i);
+                           }
+                     }
+                    cout<<count<<endl;
+              }
+  }
